2f5a5f74af97172483aac213fd8bcf03f641a53e
fff907 <59164817+fff907@users.noreply.github.com>
2025-04-08 16:36:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 <main class="container mt-5">
   <h1 class="mb-4">ようこそ！</h1>
-  <p class-"mb-5">これは自作のWordPressテーマです。</p>
+  <p class="mb-5">これは自作のWordPressテーマです。</p>
 
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <article class="mb-4">
-        <h2><php the_title(); ?></h2>
+        <h2><?php the_title(); ?></h2>
         <div><?php the_content(); ?></div>
       </article>
   　<?php endwhile; ?>
