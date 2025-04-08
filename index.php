27e393506fa98580d27d4b0ea95bd80232de8975
fff907 <59164817+fff907@users.noreply.github.com>
2025-04-08 16:30:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php get_header(); ?>
 
-<main>
-  <h1>ようこそ！</h1>
-  <p>これは自作のWordPressテーマです。</p>
+<main class="container mt-5">
+  <h1 class="mb-4">ようこそ！</h1>
+  <p class-"mb-5">これは自作のWordPressテーマです。</p>
+
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <article class="mb-4">
+        <h2><php the_title(); ?></h2>
+        <div><?php the_content(); ?></div>
+      </article>
+  　<?php endwhile; ?>
+  <?php else : ?>
+    <p>記事がありませんでした。</p>
+  <?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
