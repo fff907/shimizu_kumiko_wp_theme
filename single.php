ed391e677924e5bb9091539ba2e4f0f5e42ff4f0
fff907 <59164817+fff907@users.noreply.github.com>
2025-04-10 17:04:05 +0900

    Update single.php

--- single.php
+++ single.php
@@ -4,7 +4,9 @@
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <article class="mb-4">
       <h1 class="mb-3"><?php the_title(); ?></h1>
-      <p class="text-muted"><?php the_date(); ?></p>
+      <p class="text-muted">
+        投稿日：<?php echo get_the_date(); ?>｜投稿者：<?php the_author(); ?>｜カテゴリ：<?php the_category(', '); ?>
+      </p>
       <div><?php the_content(); ?></div>
     </article>
   <?php endwhile; endif; ?>
