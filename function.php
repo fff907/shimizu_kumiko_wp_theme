9a0c904c1f574aa9213a670f400286e603a40a80
fff907 <59164817+fff907@users.noreply.github.com>
2025-04-15 06:56:42 +0900

    Update function.php

--- function.php
+++ function.php
@@ -1,5 +1,5 @@
 <?php
 function shimizu_enqueue_styles() {
-  wp_enqueue_style('shimizu-style', get_stylesheet_uri());
+  wp_enqueue_style('shimizu-style', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'shimizu_enqueue_styles');
