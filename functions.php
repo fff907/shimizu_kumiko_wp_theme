14fa072224b6c37c7ddceb497f30885e18a37663
fff907 <59164817+fff907@users.noreply.github.com>
2025-04-15 19:23:56 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,3 +3,6 @@ function shimizu_enqueue_styles() {
   wp_enqueue_style('shimizu-style', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'shimizu_enqueue_styles');
+
+// アイキャッチ画像を有効化
+add_theme_support('post-thumbnails');
