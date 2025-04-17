50153122b3f3e773006242a6c66abb6d9edaa393
fff907 <59164817+fff907@users.noreply.github.com>
2025-04-17 17:22:55 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
-  <footer>
-    <p>&copy; <?php echo date('Y'); ?> Shimizu Kumiko Official</p>
-  </footer>
+<footer class="footer">
+  <div class="footer-copyright">
+    <small>© <?php echo date('Y'); ?> Shimizu Kumiko All Rights Reserved.</small>
+  </div>
+</footer>
 
-  <?php wp_footer(); ?>
+<!-- JS -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/hamburger.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/accordion.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/article.js"></script>
+
+<?php wp_footer(); ?>
 </body>
 </html>
