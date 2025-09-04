f64818ae2d73a4e67ab73be45d6254abc5989e3f
fff907 <59164817+fff907@users.noreply.github.com>
2025-09-04 15:53:17 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
 </footer>
 
 <!-- JS -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/hamburger.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/accordion.js"></script>
 <!-- <script src="<?php echo get_template_directory_uri(); ?>/js/article.js"></script> -->
