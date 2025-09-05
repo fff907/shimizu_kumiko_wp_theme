09bd411fc75dcf53e089d663a78f33ff2cf77ea3
fff907 <59164817+fff907@users.noreply.github.com>
2025-09-05 11:43:00 +0900

    Delete js/accordion.js

--- js/accordion.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".toggle-btn").forEach(button => {
-    button.addEventListener("click", function () {
-      const content = this.nextElementSibling;
-      content.style.display = content.style.display === "block" ? "none" : "block";
-    });
-  });
